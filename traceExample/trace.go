83215beca89d55b52a5c9901f17326ea22d27559
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2022-01-05 21:46:36 +0330

    removed some comment

--- traceExample/trace.go
+++ traceExample/trace.go
@@ -6,9 +6,7 @@ import (
 	"runtime/trace"
 )
 
-// tracing allows you to pass context through your system and evaluate where you are being held up , whether its by a third-party API call , a slow messaging quue , or an O(n2) function . tracing will help you where the bottleneck resides
 
-// the go tracing tool hooks into the goroutine scheduler so that it can produce meaningful information about goroutines.
 
 
 // install golang-misc package => in fedora run this command : dnf isntall golang-misc
